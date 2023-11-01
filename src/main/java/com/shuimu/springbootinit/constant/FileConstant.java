ac64b5f2c9166fa9542a5ff8e06f4addc910cc21
njq2210 <liu1016cc@163.com>
2023-11-01 16:22:56 +0800

    Initial commit

---
+++ src/main/java/com/shuimu/springbootinit/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.shuimu.springbootinit.constant;
+
+/**
+ * 文件常量
+ *
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "https://blog.csdn.net/qq_65142476";
+}
